a37396bfd65d8a364e2bbeaf157cbe02baadc253
zcs <zcs@169.com>
2022-07-29 14:52:27 +0800

    111

---
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,17 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+
+        System.out.println(111);
+        System.out.println(222);
+        System.out.println(333);
+
+           }
+
+}
